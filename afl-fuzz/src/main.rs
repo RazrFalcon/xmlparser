8768577ccc748bf202bcf630d4545a3cc4b7ce00
RazrFalcon <razrfalcon@gmail.com>
2018-06-14 12:49:07 +0300

    Added AFL.

---
+++ afl-fuzz/src/main.rs
@@ -0,0 +1,14 @@
+extern crate afl;
+extern crate xmlparser;
+
+use std::str;
+
+use afl::fuzz;
+
+fn main() {
+    fuzz(|data| {
+        if let Ok(text) = str::from_utf8(data) {
+            for _ in xmlparser::Tokenizer::from(text) {}
+        }
+    });
+}
