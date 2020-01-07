9f23c207b22306f932ad3b392ebae650f31bac0e
Evgeniy Reizner <razrfalcon@gmail.com>
2020-01-07 10:28:09 +0200

    Version bump.

--- src/lib.rs
+++ src/lib.rs
@@ -47,7 +47,7 @@ If you are looking for a more high-level solution - checkout
 
 #![no_std]
 
-#![doc(html_root_url = "https://docs.rs/xmlparser/0.12.0")]
+#![doc(html_root_url = "https://docs.rs/xmlparser/0.13.0")]
 
 #![forbid(unsafe_code)]
 #![warn(missing_docs)]
